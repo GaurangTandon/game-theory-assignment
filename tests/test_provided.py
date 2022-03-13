666d946428c2481164e49cd1a967c26eb501e0eb
Gaurang Tandon <1gaurangtandon@gmail.com>
2022-03-13 15:07:21 +0530

    Attempting to timeout tests

--- tests/test_provided.py
+++ tests/test_provided.py
@@ -2,19 +2,20 @@ from typing import List, Set
 
 import numpy as np
 import pygambit
+import pytest
 
 from q1 import Game
 
 
 def helper_convert_gambit_psne_to_ours(
-    gambit_g, gambit_psne: List[List[float]]
+    gambit_g: pygambit.Game, gambit_psne: List[List[float]]
 ) -> Set[List[int]]:
-    psne_strats = []
+    psne_strats: List[List[int]] = []
     processed_idx = 0
 
     for psne in gambit_psne:
         psne = list(psne)
-        strat = []
+        strat: List[int] = []
 
         for i in range(len(gambit_g.players)):
             strat.append(
@@ -30,7 +31,7 @@ def helper_convert_gambit_psne_to_ours(
 
     return set(psne_strats)
 
-
+@pytest.mark.timeout(1)
 def test_provided_games(nfg_str: str):
     nfg_str = nfg_str.strip()
     gambit_g = pygambit.Game.parse_game(nfg_str)
@@ -45,9 +46,7 @@ def test_provided_games(nfg_str: str):
     if len(utility_list) > int(1e6):
         return
 
-    g = Game(n, n_strategies, utility_list)
-
     psne_gambit_g = pygambit.nash.enumpure_solve(gambit_g)
+    g = Game(n, n_strategies, utility_list)
     psne_g = set(g.list_all_psne())
-
     assert psne_g == helper_convert_gambit_psne_to_ours(gambit_g, psne_gambit_g)
